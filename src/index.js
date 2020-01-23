6e4cac35e2004eff85bf03228ec2821eef2fa7e8
Lawrence <locaulker@gmail.com>
2020-01-23 08:17:51 -0500

    retrieved file

--- src/index.js
+++ src/index.js
@@ -2,13 +2,33 @@ import { GraphQLServer } from 'graphql-yoga'
 
 // Scalar types - String, Boolean, Int, Float, ID
 
-// 
+// Demo user data
+const users = [
+  {
+    id: '1',
+    name: 'Andrew',
+    email: 'andrew@example.com',
+    age: 27
+  },
+  {
+    id: '2',
+    name: 'Sarah',
+    email: 'sarah@example.com',
+  },
+  {
+    id: '3',
+    name: 'Mike',
+    email: 'mike@example.com',
+  },
+]
 
 // Type definitions (Application Schema)
 const typeDefs = `
   type Query {
+    users: [User]!
     me: User!
-  }
+    post: Post!
+  },
 
   type User {
     id: ID!
@@ -16,18 +36,40 @@ const typeDefs = `
     email: String!
     age: Int
   }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
+  }
 `
 
 // Resolvers (A set of functions)
 const resolvers = {
   Query: {
+    users(parent, args, ctx, info) {
+      return users
+    },
+
     me() {
       return {
         id: '123098',
         name: 'Mike',
-        email: 'mike@example.com',
+        email: 'mike@gmail.com',
+        age: 28
       }
-    }
+    },
+
+    post() {
+      return {
+        id: 'abc2323',
+        title: 'Magnificent 7',
+        body: 'Aenean lacinia bibendum nulla sed consectetur.',
+        published: true
+      }
+    },
+
   }
 }
 
